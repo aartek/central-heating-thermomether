a368ef09654126a9e1632977d632b7554a8f56a1
Artur Nowakowski <artur.nowakowski@sap.com>
2018-08-15 15:03:03 +0200

    Initial commit

---
+++ temp_with_deep_sleep.ino
@@ -0,0 +1,108 @@
+/**
+   An example showing how to put ESP8266 into Deep-sleep mode
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS D1
+
+// WiFi credentials.
+const char* WIFI_SSID = "xxxx";
+const char* WIFI_PASS = "xxxx";
+String endpoint = "http://example.com/temperature/";
+  
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+
+WiFiClientSecure wifiClient;
+char temperatureString[6];
+
+void connect() {
+
+  // Connect to Wifi.
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  // WiFi fix: https://github.com/esp8266/Arduino/issues/2186
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_OFF);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  unsigned long wifiConnectStart = millis();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    // Check to see if
+    if (WiFi.status() == WL_CONNECT_FAILED) {
+      Serial.println("Failed to connect to WiFi. Please verify credentials: ");
+      delay(10000);
+    }
+
+    delay(500);
+    Serial.println("...");
+    // Only try for 5 seconds.
+    if (millis() - wifiConnectStart > 15000) {
+      Serial.println("Failed to connect to WiFi");
+      return;
+    }
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  Serial.println();
+
+  DS18B20.begin();
+}
+
+float getTemperature() {
+  float temp;
+  do {
+    DS18B20.requestTemperatures(); 
+    temp = DS18B20.getTempCByIndex(0);
+    delay(100);
+  } while (temp == 85.0 || temp == (-127.0));
+  return temp;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
+
+  // Wait for serial to initialize.
+  while (!Serial) { }
+
+  Serial.println("Device Started");
+  Serial.println("-------------------------------------");
+  Serial.println("Running Deep Sleep Firmware!");
+  Serial.println("-------------------------------------");
+
+  connect(); //connect to wifi
+
+  float temperature = getTemperature();
+  dtostrf(temperature, 2, 2, temperatureString);
+  // send temperature to the serial console
+  Serial.println(temperatureString);
+  
+  HTTPClient http;
+  http.begin(String(endpoint + temperatureString));
+
+  int httpCode = http.GET();
+  Serial.println(httpCode);
+  http.end();
+
+  Serial.println("Going into deep sleep for 20 seconds");
+  ESP.deepSleep(20e6); // 20e6 is 20 microseconds
+}
+
+void loop() {
+}
